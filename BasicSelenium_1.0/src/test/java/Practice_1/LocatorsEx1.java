481d6b2325190677aacf29ee47f83866fd4e72a5
ubkale55 <ubkale55@gmail.com>
2025-08-01 17:28:41 +0530

    Update LocatorsEx1.java

--- BasicSelenium_1.0/src/test/java/Practice_1/LocatorsEx1.java
+++ BasicSelenium_1.0/src/test/java/Practice_1/LocatorsEx1.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorsEx1 {
 
+	//pull it
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         
